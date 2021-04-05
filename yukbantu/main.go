61537e386e416b4c602e6a2db26a06f5ca138ba8
unknown <hary.teguh@ui.ac.id>
2021-04-05 11:56:49 +0700

    First Commit

---
+++ yukbantu/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
+	dsn := "root:@tcp(127.0.0.1:3306)/yukbantu?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+}
