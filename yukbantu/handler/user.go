91997bf9eb675a64253e4dc7074b34b690964c32
Hary Ridart <ridarth@gmail.com>
2021-04-09 18:31:14 +0700

    Error handling

--- yukbantu/handler/user.go
+++ yukbantu/handler/user.go
@@ -21,11 +21,15 @@ func (h *userHandler) RegisterUser(c *gin.Context) {
 	var input user.RegisterUserInput
 	err := c.ShouldBindJSON(&input)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, nil)
+		response := helper.APIResponse("Account Registration Failed!", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
 	}
 	newUser, err := h.userService.RegisterUser(input)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, nil)
+		response := helper.APIResponse("Account Registration Failed!", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
 	}
 	formatter := user.FormatUser(newUser, "this is the token")
 
