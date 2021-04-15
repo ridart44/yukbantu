cc404b234ab14164a9f5f9e3b06b8878a835b6db
Hary Ridart <ridarth@gmail.com>
2021-04-15 12:58:03 +0700

    handler for Upload Avatar API

--- handler/user.go
+++ handler/user.go
@@ -112,4 +112,35 @@ func (h *userHandler) UploadAvatar(c *gin.Context) {
 	// service call repo
 	// repo get find data to update by ID
 	// repo update user data, store file location
+	file, err := c.FormFile("avatar")
+	if err != nil {
+		data := gin.H{"is_uploaded": false}
+		response := helper.APIResponse("Failed to upload avatar image", http.StatusBadRequest, "error", data)
+
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+	path := "images/" + file.Filename
+	err = c.SaveUploadedFile(file, path)
+	if err != nil {
+		data := gin.H{"is_uploaded": false}
+		response := helper.APIResponse("Failed to upload avatar image", http.StatusBadRequest, "error", data)
+
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+	userID := 1
+	_, err = h.userService.SaveAvatar(userID, path)
+	if err != nil {
+		data := gin.H{"is_uploaded": false}
+		response := helper.APIResponse("Failed to upload avatar image", http.StatusBadRequest, "error", data)
+
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+	data := gin.H{"is_uploaded": true}
+	response := helper.APIResponse("Avatar Image Successfully Uploaded", http.StatusOK, "error", data)
+
+	c.JSON(http.StatusOK, response)
+
 }
